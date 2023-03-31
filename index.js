a83f5fbfa3fa4cdc895a21d3cfebba4d62a9741e
Alex Corrado <chkn@hey.com>
2023-03-31 22:01:01 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const core = require('@actions/core');
+const http = require('@actions/http-client');
+
+const body = core.getInput('body', { required: true });
+const regex = /^View details in Rollbar: \[https:\/\/rollbar.com\/[^/]+\/[^/]+\/items\/([^/]+)/m;
+
+const main = async () => {
+	const token = process.env.ROLLBAR_TOKEN;
+	if (!token) {
+		throw new Error('ROLLBAR_TOKEN environment variable is not set');
+	}
+
+	const match = regex.exec(body);
+	if (match) {
+		const itemCounter = match[1];
+		core.debug(`Found Rollbar item counter: ${itemCounter}`);
+
+		const rollbar = new http.HttpClient(undefined, undefined, { allowRedirects: false });
+
+		const response1 = await rollbar.getJson(
+			`https://api.rollbar.com/api/1/item_by_counter/${itemCounter}`,
+			{
+				"X-Rollbar-Access-Token:": token,
+				[http.Headers.Accept]: "application/json"
+			}
+		);
+		const itemId = response.result?.itemId;
+		if (!itemId) {
+			throw new Error(`Could not find item ID for counter ${itemCounter}`);
+		}
+
+		const response2 = await rollbar.patch(
+			`https://api.rollbar.com/api/1/item/${itemId}`,
+			'{"status": "resolved"}',
+			{
+				"X-Rollbar-Access-Token:": token,
+				[http.Headers.ContentType]: "application/json"
+			}
+		);
+		if (response2.statusCode !== 200) {
+			throw new Error(`Could not resolve item ${itemId}: ${response2.message}`);
+		} else {
+			core.info(`Resolved item ${itemId}`);
+		}
+	} else {
+		core.info('No Rollbar item link found');
+	}
+};
+
+main().catch(error => {
+	core.setFailed(error.message);
+});
